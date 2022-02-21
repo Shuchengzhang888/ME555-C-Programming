aea2a4fa075cfaa74640751ecf8bd3bee88b925c
sz255 <sz255@duke.edu>
2022-02-21 16:51:19 -0500

    044

---
+++ 044_eval2/stats.c
@@ -0,0 +1,21 @@
+#include "stats.h"
+#include <stdio.h>
+#include <stdlib.h>
+// any other headers you need here
+
+stat_t parseLine(const char * line) {
+  // STEP 1: write me
+  
+}
+
+double calcGamePerformance(stat_t * statarr, unsigned long * pointarr, size_t nplayers) {
+  // STEP 2: write me
+}
+
+void addGameStats(stat_t * statarr, unsigned long * pointarr, size_t nplayers) {
+  // STEP 3: write me
+}
+
+void printBreakout(stat_t * statarr, unsigned long * pointarr, size_t nplayers) {
+  // STEP 4: write me
+}
