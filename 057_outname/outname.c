0c1afc39811102da3a6e3507652d78a9741ca5a7
sz255 <sz255@duke.edu>
2022-02-24 23:43:53 -0500

    057

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -7,8 +7,10 @@ char * computeOutputFileName(const char * inputName) {
   //WRITE ME
   size_t len = strlen(inputName);
   char * result;
+  //strndup: return fixed length(len) string from first parameters(inputname) 
   result = strndup(inputName, len);
   result = realloc(result, (len + 8) * sizeof(*result));
+  //copy ".counts\0" to inputName in the "result+len" place
   strcpy(result + len, ".counts\0");
   return result;
 }
