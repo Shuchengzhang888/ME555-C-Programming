d6395fe79d37e66286121d8e91ec3db4f39f6999
sz255 <sz255@duke.edu>
2022-02-25 16:59:34 -0500

    058

--- 058_counts/counts.h
+++ 058_counts/counts.h
@@ -2,15 +2,18 @@
 #define __COUNTS_H__
 struct _one_count_t {
   //DEFINE ME
-
+  char * string;
+  int cnt;
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
   //DEFINE ME
+  one_count_t ** array;
+  int size;
+  int cnt_unknown;
 };
 typedef struct _counts_t counts_t;
-
 counts_t * createCounts(void);
 void addCount(counts_t * c, const char * name);
 void printCounts(counts_t * c, FILE * outFile);
