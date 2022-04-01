0a2c448319443cc0477893f5fa1e6c9e45887807
sz255 <sz255@duke.edu>
2022-03-31 23:56:55 -0400

    089

--- 089_parse_input/parse.cpp
+++ 089_parse_input/parse.cpp
@@ -11,106 +11,130 @@
 #include <algorithm>
 #include <vector>
 
-class invalid_input : public std::exception {
-  // override what() here
-public:
-  const char * what () const throw () {
-    return "Wrong input format";
-  }
-};
-void splitString(std::string& s, std::vector<std::string>& v, std::string& c)
-{
-  std::string::size_type pos1, pos2;
-  pos2 = s.find(c);
-  pos1 = 0;
-  if(pos2 == s.npos && c == ": "){
-    v.push_back(s.substr(pos1,s.size()-1));
-    v.push_back("");
-  }
-  if(pos2 == s.npos && c == ", "){
-    v.push_back(s.substr(pos1,s.size()));
-    v.push_back("");
-  }
-  else{
-    while(std::string::npos != pos2)
-    {
-      v.push_back(s.substr(pos1, pos2-pos1));
-   
-      pos1 = pos2 + c.size();
-      pos2 = s.find(c, pos1);
-      if(c == ": "){
-        break;
-      }
+//Override std::exception::what() here
+class invalid_input : public std::exception{
+  public:
+    const char * what() const throw(){
+        return "Wrong Input Format. The wrong line is ";
     }
-    if(pos1 != s.length())
-      v.push_back(s.substr(pos1));
-  }
-}
-item_t * parseLine(std::string & line) {
-  // write me
-  //item_t* it = (item_t*)malloc(sizeof(*it));//WRONG!!!!!
-  item_t* it = new item_t();
-  std::vector<std::string> v;
-  std::string c = ": ";
-  splitString(line,v,c);
-  it->first = v[0];
-  std::vector<std::string> w;
-  c = ", ";
-  splitString(v[1],w,c);
-  it->second = w;
-  return it;  
-}
-//Print
-void print(std::vector<item_t*> &item){
-  for(size_t i=0;i<item.size();i++){
-    std::cout<<"key = "<<item[i]->first<<std::endl;
-    std::cout<<"  values = ";
-    if(item[i]->second[1]==""){
-      std::cout<<item[i]->second[0]<<std::endl;
+};
+class invalid_command_line : public std::exception{
+  public:
+    const char * what() const throw(){
+        return "Invalid Command Line.";
+    }
+};
+class invalid_file : public std::exception{
+  public:
+    const char * what() const throw(){
+        return "Invalid File.";
+    }
+};
+
+
+void split_line(const std::string & line, std::vector<std::string> & vec){
+    std::string colon = ":";
+    std::string comma = ",";
+    size_t colon_index = line.find(colon);
+    size_t comma_index = line.find(comma);
+    if(colon_index == line.size() - 1){
+        vec.push_back(line.substr(0, colon_index));
+        vec.push_back("");
+        vec.push_back("");
     }
     else{
-      std::cout<<item[i]->second[0];
-      for(size_t j=1;j<item[i]->second.size();j++){
-        std::cout<<"; "<<item[i]->second[j];
-      }
-      std::cout<<std::endl;
+        if(comma_index == line.npos){
+            vec.push_back(line.substr(0, colon_index));
+            vec.push_back(line.substr(colon_index + 2, line.size()));
+            vec.push_back("");
+        }
+        else{
+            vec.push_back(line.substr(0, colon_index));
+            vec.push_back(line.substr(colon_index + 2, comma_index - 4));
+            vec.push_back(line.substr(comma_index + 2, line.size()));
+        }
+    }
+}
+
+item_t * parseLine(const std::string & line){
+    item_t * item = new item_t;
+    std::vector<std::string> vec;
+
+    split_line(line, vec);
+    //pair是将2个数据组合成一组数据，当需要这样的需求时就可以使用pair，如stl中的map就是将key和value放在一起来保存。
+    //另一个应用是，当一个函数需要返回2个数据的时候，可以选择pair。
+    //pair将一对值(T1和T2)组合成一个值，这一对值可以具有不同的数据类型（T1和T2），两个值可以分别用pair的两个公有函数first和second访问。
+    item->first = vec[0];
+    std::vector<std::string>::iterator it = vec.begin();
+    vec.erase(it);
+    item->second= vec;
+
+    return item;
+}
+
+void print_item_t(std::vector<item_t *> & items){
+    for(size_t i = 0; i < items.size(); i++){
+    std::cout << "key = " << items[i]->first << std::endl;
+    std::cout << "  values = ";
+    if(items[i]->second[1] == ""){
+      std::cout << items[i]->second[0] << std::endl;
+    }
+    else{
+      std::cout << items[i]->second[0] << "; "<<items[i]->second[1] << std::endl;
     }
   }
+
 }
+
 int main(int argc, char ** argv){
-  if (argc != 2) {
-    fprintf(stderr, "Input Error\n");
-    return EXIT_FAILURE;
-  }
-  std::ifstream ifs(argv[1]);
-  if(!ifs.is_open()){
-    perror("Could not open file");
-   	return EXIT_FAILURE;
-  }
-  std::string line;
-  std::vector<item_t*> item;
-/*Practice dynamic allocation by allocating an item_t *
-  for each line read, storing these pointers in a vector. */
-  while(getline(ifs,line)){
-    try {
-      if(line.find(":")== line.npos){
-        throw invalid_input();
-      }
-      /*else if(line.find_first_of(":") != line.find_last_of(":")){
-        throw invalid_input();
-      }*/
+    try{
+        if(argc != 2){
+            throw invalid_command_line();
+        }
     }
-    catch (invalid_input & e) {
-      std::cerr << e.what() << std::endl;
-      continue;
+    catch (invalid_command_line & e){
+        std::cerr << e.what()<< std::endl;
+        exit(EXIT_FAILURE);
     }
-    item_t * it = parseLine(line);
-    item.push_back(it);
-  } 
-  print(item);
-  for(size_t i=0;i<item.size();i++){
-    delete item[i];
-  }
-  return EXIT_SUCCESS;
-  
+
+    std::ifstream f(argv[1]);
+    try{
+        if(!f.is_open()){
+            throw invalid_file();
+        }
+    }
+    catch (invalid_file & e){
+        std::cerr << e.what()<< std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::vector<item_t *> items;
+    std::string line;
+    size_t count = 0;
+    //Practice dynamic allocation by allocating an item_t *
+    //for each line read, storing these pointers in a vector.
+    while(std::getline(f, line)){
+        count ++;
+        try{
+            //string::find
+            //The position of the first character of the first match.
+            //If no matches were found, the function returns string::npos.
+            if(line.find(":") == line.npos){
+                throw invalid_input();
+            }
+        }
+        catch (invalid_input & e){
+            std::cerr << e.what() << count << "."<< std::endl;
+            continue;
+        }
+        item_t * item = parseLine(line);
+        items.push_back(item);
+    }
+
+    print_item_t(items);
+    for(size_t i = 0; i < items.size(); i++){
+        delete items[i];
+    }
+    return EXIT_SUCCESS;
 }
+
