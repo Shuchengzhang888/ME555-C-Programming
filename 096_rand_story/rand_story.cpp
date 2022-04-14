7805703e2501857a22527e2dfda24263c66ad953
sz255 <sz255@duke.edu>
2022-04-14 19:17:15 -0400

    096

--- 096_rand_story/rand_story.cpp
+++ 096_rand_story/rand_story.cpp
@@ -1,124 +1,191 @@
-#include <map>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cmath>
 #include <algorithm>
+#include <map>
 #include <vector>
-void splitString(std::string& s, std::vector<std::string>& v, std::string& c)
-{
-  std::string::size_type pos1, pos2;
-  pos2 = s.find(c);
-  pos1 = 0;
-  while(std::string::npos != pos2)
-  {
-    v.push_back(s.substr(pos1, pos2-pos1));
+#include <exception>
+
+#define output_name "output.txt"
+
+class invalid_command_line : public std::exception{
+  public:
+    const char * what() const throw(){
+        return "Invalid Command Line.";
+    }
+};
+class invalid_file : public std::exception{
+  public:
+    const char * what() const throw(){
+        return "Invalid File.";
+    }
+};
+class invalid_seed : public std::exception{
+  public:
+    const char * what() const throw(){
+        return "Invalid Seed Number.";
+    }
+};
+class invalid_input : public std::exception{
+  public:
+    const char * what() const throw(){
+        return "Invalid Input Words.";
+    }
+};
+class invalid_type : public std::exception{
+  public:
+    const char * what() const throw(){
+        return "Invalid Type.";
+    }
+};
+
+void split_line(const std::string & line, std::vector<std::string> & vec){
+    std::string colon = ":";
+    size_t colon_index = line.find(colon);
+    try{
+        if(colon_index == line.npos){
+            throw invalid_input();
+        }
+    }
+    catch (invalid_input & e){
+        std::cerr << e.what()<< std::endl;
+        exit(EXIT_FAILURE);
+    }
+    vec.push_back(line.substr(0, colon_index));
+    vec.push_back(line.substr(colon_index + 1, line.size()));
+}
+
+
+std::map<std::string, std::vector<std::string> > read_words(std::ifstream & f_words){
+    std::map<std::string, std::vector<std::string> > words;
+    std::string line;
+    while(std::getline(f_words, line)){
+        std::vector<std::string> vec;
+        split_line(line, vec);
+        words[vec[0]].push_back(vec[1]);
+    }
+    return words;
+}
+
+void replace_words(std::string & line, std::map<std::string, std::vector<std::string> > & words, std::vector<std::string> & used_words){
+    std::string underscore = "_";
  
-    pos1 = pos2 + c.size();
-    pos2 = s.find(c, pos1);
-  }
-  if(pos1 != s.length())
-    v.push_back(s.substr(pos1));
+    while(line.find(underscore) != line.npos){
+        std::string::size_type pos1, pos2;
+        std::string type;
+        std::string rand_words;
+
+        pos1 = line.find(underscore);
+        pos2 = line.find(underscore, pos1 + 1);
+        type = line.substr(pos1 + 1, pos2 - pos1 - 1);
+        //std::string.c_str() returns const char *
+        if(atoi(type.c_str()) == 0){
+            //type situation
+            try{
+                if(words.find(type) == words.end()){
+                    throw invalid_type();
+                }
+                else{
+                    int r = rand() % words[type].size();
+                    rand_words = words[type][r];
+                }
+            }
+            catch (invalid_type & e){
+                std::cerr << e.what()<< std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        else{
+            //number situation
+            try{
+                size_t num = atoi(type.c_str());
+                if(num < 1 || num > used_words.size()){
+                    throw invalid_type();
+                }
+                rand_words = used_words[used_words.size() - num];
+            }
+            catch (invalid_type & e){
+                std::cerr << e.what()<< std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        line = line.replace(pos1, pos2 - pos1 + 1, rand_words);
+        used_words.push_back(rand_words);
+    }
 }
-std::map<std::string, std::vector<std::string> > readWords(char* filename){
-  std::map<std::string, std::vector<std::string> > M;
-  std::ifstream ifs(filename);
-  if(!ifs.is_open()){
-    perror("Could not open Words");
-   	exit(EXIT_FAILURE);
-  }
-  std::string line;
-  while(std::getline(ifs,line)){
-    std::vector<std::string> v;
-    std::string c = ":";
-    splitString(line,v,c);
-    //Words file with invalid syntax: No colon
-    if(v.size() != 2){
-      perror("Words file with invalid syntax: No colon");
-   	  exit(EXIT_FAILURE);
+
+
+std::vector<std::string> write_story(std::ifstream & f_story, std::map<std::string, std::vector<std::string> > words){
+    std::vector<std::string> story;
+    std::string line;
+    std::vector<std::string> used_words;
+
+    while(std::getline(f_story, line)){
+        replace_words(line, words, used_words);
+        story.push_back(line);
     }
-    M[v[0]].push_back(v[1]);
-  }
-  //debug test
-  /*std::map<std::string, std::vector<std::string> >::iterator it;
-  for(it = M.begin();it != M.end();++it){
-    std::cout<<it->first<<std::endl;
-    for(size_t i=0;i<it->second.size();i++){
-      std::cout<<it->second[i]<<" ";
-    }
-    std::cout<<std::endl;
-  }*/
-  return M;
+    return story;
 }
-void lineReplace(std::string &line,std::map<std::string, std::vector<std::string> > &M){
-  std::string l;//random name
-  std::string::size_type pos1, pos2;
-  std::string temp;
-  while(line.find("_") != line.npos){
-    pos1 = line.find("_");
-    pos2 = line.find("_",pos1+1);
-    //input error like _time
-    if(pos2 == line.npos){
-      perror("Input Error:lack second _");
-   	  exit(EXIT_FAILURE);
+
+void print_story(std::vector<std::string> story){
+    std::ofstream OutFile(output_name);
+    std::vector<std::string>::iterator iter;
+    for(iter = story.begin(); iter != story.end(); ++iter){
+        if(iter == story.end() - 1){
+            OutFile << *iter << std::endl;
+        }
+        else{
+            OutFile << *iter << "\n";
+        }
     }
-    temp = line.substr(pos1+1,pos2-pos1-1);
-    //std::cout<<"temp = "<<temp<<std::endl;//debug test
-    if(atoi(temp.c_str()) == 0){
-      //Story file with nonexistent category
-      if(M.count(temp) == 0){
-        perror("Story file with nonexistent category");
-   	    exit(EXIT_FAILURE);
-      }
-      int r = rand() % M[temp].size();//random number
-      //std::cout<<"random number= "<<r<<std::endl;//debug test
-      l = (M[temp])[r];
-    }
-    else{
-      int n = M["used"].size()-atoi(temp.c_str());
-      if(n < 0){
-        perror("Number refers past beginning of story");
-   	    exit(EXIT_FAILURE);
-      }
-      l = (M["used"])[n]; 
-    }
-    line = line.replace(pos1,temp.length()+2,l);
-    M["used"].push_back(l);//keep track of used words
-  }
+    
+    OutFile.close();
 }
-std::vector<std::string> readStory(char* filename,std::map<std::string, std::vector<std::string> > M){
-  std::vector<std::string> S;
-  std::ifstream ifs(filename);
-  if(!ifs.is_open()){
-    perror("Could not open Story");
-   	exit(EXIT_FAILURE);
-  }
-  std::string line;
-  while(std::getline(ifs,line)){
-    lineReplace(line,M);
-    std::cout<<line<<std::endl;//debug test
-    S.push_back(line);
-  }
-  return S;
-}
+
 int main(int argc, char ** argv){
-  if(argc != 4) {
-    fprintf(stderr, "Input Error:3 command line arguments\n");
-    return EXIT_FAILURE;
-  }
-  int r = atoi(argv[3]);//random seed
-  if(r <= 0) {
-    fprintf(stderr, "Input Error:random seed must be 1 or greater\n");
-    return EXIT_FAILURE;
-  }
-  srand(r);//set the random seed
-  //(1) read and store the categories and words in a map
-  std::map<std::string, std::vector<std::string> > M;
-  M = readWords(argv[2]);
-  //(2) read the story template, parsing each blank and selecting an appropriate word.
-  std::vector<std::string> S;//Story
-  S = readStory(argv[1],M);
-  return EXIT_SUCCESS;
+    try{
+        if(argc != 4){
+            throw invalid_command_line();
+        }
+        std::ifstream f_story(argv[1]);
+        std::ifstream f_words(argv[2]);
+        if(!(f_story.is_open() && f_words.is_open())){
+            throw invalid_file();
+        }
+        //set random seed
+        int r = atoi(argv[3]);
+        if(r <= 0){
+            throw invalid_seed();
+        }
+        //For every different seed value used in a call to srand, the pseudo-random number generator 
+        // can be expected to generate a different succession of results in the subsequent calls to rand.
+        //srand(r)会影响rand()的值
+        srand(r);
+	
+	//read words and save them to map
+        std::map<std::string, std::vector<std::string> > words;
+        words = read_words(f_words);
+	//read story template and write story to vector
+        std::vector<std::string> story;
+        story = write_story(f_story, words);
+        print_story(story);
+        f_story.close();
+        f_words.close();
+    }
+    catch (invalid_command_line & e){
+        std::cerr << e.what()<< std::endl;
+        exit(EXIT_FAILURE);
+    }
+    catch (invalid_file & e){
+        std::cerr << e.what()<< std::endl;
+        exit(EXIT_FAILURE);
+    }
+    catch(invalid_seed & e){
+        std::cerr << e.what()<< std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
 }
